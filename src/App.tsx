1ecce4d16f69c5b830c585756bb3ccc84d23d036
Jungkyu Yang <didwjd8590412@gmail.com>
2025-05-14 13:54:22 +0900

    init: 초기 환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <div>프로젝트 환경설정</div>
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //darkMode: ['class'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        noto: ['var(--font-family)'],
+      },
+    },
+  },
+  corePlugins: {
+    preflight: true,
+  },
+  plugins: [require('@tailwindcss/postcss')],
+}
