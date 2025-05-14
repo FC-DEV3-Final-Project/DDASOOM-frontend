3cb83417f1e40948bf6d2ccafdc4e88086777a8b
j0n0m2 <dbsk8223@naver.com>
2025-05-15 00:24:13 +0900

    chore: StoryBook 버튼 컴포넌트 예제 Type 명시

--- src/stories/Button_example.stories.tsx
+++ src/stories/Button_example.stories.tsx
@@ -1,7 +1,8 @@
-import Button from '../shared/components/Button_example.tsx'
+import Button, { type ButtonProps } from '../shared/components/Button_example.tsx'
+import type { Meta, StoryObj } from '@storybook/react'
 
 // 스토리북에서 사용할 메타데이터를 설정
-export default {
+const meta: Meta<ButtonProps> = {
   title: 'Button', // 스토리북에서 보여줄 제목
   component: Button,
   tags: ['autodocs'], // 스토리북에서 자동으로 문서화
@@ -31,11 +32,13 @@ export default {
   },
 }
 
+export default meta
+
 // 스토리를 만드는 방식 두 가지
 
 // 1. 스토리북 공식 문서에 나온 방식
 // 스토리 안에서 args를 직접 지정하는 방식
-export const Default = {
+export const Default: StoryObj<ButtonProps> = {
   args: {
     label: 'Button',
     variant: 'primary',
@@ -43,13 +46,13 @@ export const Default = {
     disabled: false,
   },
 }
-export const Secondary = {
+export const Secondary: StoryObj<ButtonProps> = {
   args: {
     ...Default.args, // default args를 복사
     variant: 'secondary',
   },
 }
-export const Disabled = {
+export const Disabled: StoryObj<ButtonProps> = {
   args: {
     ...Default.args,
     disabled: true,
@@ -60,22 +63,22 @@ export const Disabled = {
 // const Template: StoryFn<ButtonProps> = (args) => <Button {...args} />
 
 // 스토리북에서 보여줄 스토리
-// export const Primary = Template.bind({})
+// export const Default = Template.bind({})
 // export const Secondary = Template.bind({})
 // export const Disabled = Template.bind({})
 
 // Primary, Secondary, Disabled 스토리에서 보여줄 args
-// Primary.args = {
-//   label: 'Primary',
+// Default.args = {
+//   label: 'Button',
 //   variant: 'primary',
+//   size: 'medium',
+//   disabled: false,
 // }
-
 // Secondary.args = {
-//   label: 'Secondary',
+//   ...Default.args, // default args를 복사
 //   variant: 'secondary',
 // }
-
 // Disabled.args = {
-//   label: 'Disabled',
+//   ...Default.args,
 //   disabled: true,
 // }
