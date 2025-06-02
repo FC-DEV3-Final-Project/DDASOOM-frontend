e0028111e0be6b9d53de4147c051ddd039c38e22
j0n0m2 <dbsk8223@naver.com>
2025-06-02 14:02:41 +0900

    feat : 아이콘 추가 및 바로가기 버튼과 기증자수 대시보드 스타일 수정

--- src/pages/home/components/QuickLinks.tsx
+++ src/pages/home/components/QuickLinks.tsx
@@ -1,42 +1,46 @@
 import React from 'react'
 import { HandHeart } from 'lucide-react'
 
+const ecgHeartIcon = '/public/icon/ecg_heart.svg'
+const identityIcon = '/public/icon/identity.svg'
+const mailIcon = '/public/icon/mail.svg'
+
 const QuickLinks: React.FC = () => {
   return (
     <section className="mx-auto flex w-full max-w-[1300px] flex-col items-start gap-[80px] self-stretch bg-white/80 px-[20px] backdrop-blur-lg">
-      <div className="flex flex-wrap justify-between gap-[18px]">
-        <button className="inline-flex items-center justify-between gap-[14px] rounded-[100px] border-[2.5px] border-[#EAEAEA] bg-white px-[28px] py-[20px] shadow-md hover:shadow-lg focus:outline-none">
+      <div className="flex flex-wrap gap-[18px]">
+        <button className="inline-flex items-center justify-between gap-[14px] rounded-[100px] border-[2.5px] border-[#EAEAEA] bg-white px-[28px] py-[20px] transition-all duration-300 ease-in-out hover:shadow-md focus:outline-none">
           <div className="flex h-[30px] w-[33px] flex-shrink-0 items-center justify-center text-red-500">
             <HandHeart size={40} strokeWidth={1.5} />
           </div>
-          <span className="text-[24px] leading-[36px] font-bold whitespace-nowrap text-black">
+          <span className="text-gray-80 mobile:text-[14px] desktop:text-[24px] text-[14px] leading-[36px] font-bold whitespace-nowrap">
             기증자 예우
           </span>
           <span className="text-gray-80 text-lg">&gt;</span>
         </button>
-        <button className="inline-flex items-center justify-between gap-[14px] rounded-[100px] border-[2.5px] border-[#EAEAEA] bg-white px-[28px] py-[20px] shadow-md hover:shadow-lg focus:outline-none">
+        <button className="inline-flex items-center justify-between gap-[14px] rounded-[100px] border-[2.5px] border-[#EAEAEA] bg-white px-[28px] py-[20px] transition-all duration-300 hover:shadow-md focus:outline-none">
           <div className="flex h-[30px] w-[33px] flex-shrink-0 items-center justify-center text-red-500">
-            {/* 아이콘 자리 */}
+            <img src={ecgHeartIcon} alt="" width={33} height={33} />
           </div>
-          <span className="text-[24px] leading-[36px] font-bold whitespace-nowrap text-black">
+          <span className="text-gray-80 mobile:text-[14px] desktop:text-[24px] text-[14px] leading-[36px] font-bold whitespace-nowrap">
             장기·조직기증 소개
           </span>
           <span className="text-gray-80 text-lg">&gt;</span>
         </button>
-        <button className="inline-flex items-center justify-between gap-[14px] rounded-[100px] border-[2.5px] border-[#EAEAEA] bg-white px-[28px] py-[20px] shadow-md hover:shadow-lg focus:outline-none">
+        <button className="inline-flex items-center justify-between gap-[14px] rounded-[100px] border-[2.5px] border-[#EAEAEA] bg-white px-[28px] py-[20px] transition-all duration-300 hover:shadow-md focus:outline-none">
           <div className="flex h-[30px] w-[33px] flex-shrink-0 items-center justify-center text-red-500">
-            {/* 아이콘 자리 */}
+            <img src={identityIcon} alt="" width={33} height={33} />
           </div>
-          <span className="text-[24px] leading-[36px] font-bold whitespace-nowrap text-black">
+          <span className="text-gray-80 mobile:text-[14px] desktop:text-[24px] text-[14px] leading-[36px] font-bold whitespace-nowrap">
             기증희망등록
           </span>
           <span className="text-gray-80 text-lg">&gt;</span>
         </button>
-        <button className="inline-flex items-center justify-between gap-[14px] rounded-[100px] border-[2.5px] border-[#EAEAEA] bg-white px-[28px] py-[20px] shadow-md hover:shadow-lg focus:outline-none">
+        <button className="inline-flex items-center justify-between gap-[14px] rounded-[100px] border-[2.5px] border-[#EAEAEA] bg-white px-[28px] py-[20px] hover:shadow-md focus:outline-none">
           <div className="flex h-[30px] w-[33px] flex-shrink-0 items-center justify-center text-red-500">
-            {/* 아이콘 자리 */}
+            <img src={mailIcon} alt="" width={33} height={33} />
           </div>
-          <span className="text-[24px] leading-[36px] font-bold whitespace-nowrap text-black">
+          <span className="text-gray-80 mobile:text-[14px] desktop:text-[24px] text-[14px] leading-[36px] font-bold whitespace-nowrap">
             생명나눔 희망우체통
           </span>
           <span className="text-gray-80 text-lg">&gt;</span>
--- src/pages/home/components/StatsSection.tsx
+++ src/pages/home/components/StatsSection.tsx
@@ -2,20 +2,21 @@ import React from 'react'
 
 const StatsSection: React.FC = () => {
   return (
-    <section className="flex items-center justify-between rounded-lg bg-[#F2F2F2] p-8">
+    <section className="bg-gray-5 flex items-center justify-between rounded-[40px] p-8">
       <div>
-        <h2 className="mb-2 text-2xl font-bold text-[#212121]">2025년 누적 기증자 수</h2>
-        <p className="mb-4 text-base text-[#616161]">2025년 5월 26일(주간) 기준</p>
-        <div className="flex space-x-10">
+        <h2 className="text-gray-95 mb-2 text-2xl font-bold">2025년</h2>
+        <h2 className="mb-2 text-2xl font-bold text-green-50">누적 기증자 수</h2>
+        <p className="text-gray-40 mb-4 text-base">2025년 5월 26일(주간) 기준</p>
+        <div className="mt-[40px] flex space-x-10">
           <div>
-            <p className="text-lg font-semibold text-[#424242]">장기</p>
-            <p className="text-5xl font-bold text-[#0D47A1]">
+            <p className="text-gray-60 text-lg font-semibold">장기</p>
+            <p className="text-gray-80 text-5xl font-bold">
               136<span className="text-2xl font-normal">명</span>
             </p>
           </div>
           <div>
-            <p className="text-lg font-semibold text-[#424242]">조직</p>
-            <p className="text-5xl font-bold text-[#388E3C]">
+            <p className="text-gray-60 text-lg font-semibold">조직</p>
+            <p className="text-gray-80 text-5xl font-bold">
               54<span className="text-2xl font-normal">명</span>
             </p>
           </div>
