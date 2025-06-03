fb3cdb118e20316da6d3046f1e7920bd0605764a
j0n0m2 <dbsk8223@naver.com>
2025-06-03 12:25:29 +0900

    fix : 유튜브 컴포넌트 스타일 수정

--- src/pages/home/components/KodaStorySection.tsx
+++ src/pages/home/components/KodaStorySection.tsx
@@ -3,13 +3,25 @@ import React from 'react'
 const KodaStorySection: React.FC = () => {
   return (
     <div className="flex flex-col">
-      <h2 className="mb-6 text-2xl font-bold text-[#212121]">KODA 이야기 &gt;</h2>
-      <p className="mb-8 text-base text-[#616161]">
-        한국장기조직기증원의 새로운 소식을 만나보세요.
-      </p>
-      {/* TODO: 유튜브 썸네일 및 링크 삽입 */}
-      <div className="flex aspect-video w-full items-center justify-center bg-gray-300 text-gray-600">
-        유튜브 영상 플레이스홀더
+      <section className="header">
+        <a className="mb-1 inline-flex cursor-pointer items-center gap-3" href="/event">
+          <h2 className="text-gray-95 inline text-2xl font-bold">KODA 이야기</h2>
+          <img src="/icon/Arrow.svg" alt="" width={24} height={24} />
+        </a>
+        <p className="text-gray-60 mb-7 text-[19px] font-normal">
+          한국장기조직기증원의 새로운 소식을 만나보세요.
+        </p>
+      </section>
+      <div className="flex aspect-video w-full items-center justify-center rounded-[30px] bg-gray-300 text-gray-600">
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/hVJtnFfYvHw"
+          title="KODA 이야기"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="rounded-[30px]"
+        ></iframe>
       </div>
     </div>
   )
--- src/pages/home/components/StorySection.tsx
+++ src/pages/home/components/StorySection.tsx
@@ -6,38 +6,34 @@ const StorySection: React.FC = () => {
       <section className="header">
         <a className="mb-1 inline-flex cursor-pointer items-center gap-3" href="/event">
           <h2 className="text-gray-95 inline text-2xl font-bold">생명나눔 페이지</h2>
-          <img src="/public/icon/Arrow.svg" alt="" width={24} height={24} />
+          <img src="/icon/Arrow.svg" alt="" width={24} height={24} />
         </a>
-        <p className="text-gray-60 mb-[28px] text-[19px] font-normal">
+        <p className="text-gray-60 mb-7 text-[19px] font-normal">
           그리움과 사랑을 담아 소중한 이들을 기억하는 공간입니다.
         </p>
       </section>
-      <section className="flex gap-8">
+      <section className="flex gap-6">
         <div className="flex h-[327px] w-[245px] overflow-hidden rounded-[20px]">
-          <img
-            src="/public/story_image.png"
-            alt="생명 나눔 이야기 이미지"
-            className="h-auto w-full"
-          />
+          <img src="/story_image.png" alt="생명 나눔 이야기 이미지" className="h-auto w-full" />
         </div>
         <div className="flex w-[321px] flex-col gap-2">
           <p className="text-gray-80 bg-gray-10 w-full cursor-pointer overflow-hidden rounded-[100px] px-[18px] py-[10px] text-[19px] font-bold overflow-ellipsis whitespace-nowrap">
-            한 사람의 따뜻한 선택이, 누군가의 내일을 다시...
+            한 사람의 따뜻한 선택이, 누군가의 내일을 다시 뛰게 합니다.
           </p>
           <p className="w-full cursor-pointer overflow-hidden px-[18px] py-[10px] text-[19px] overflow-ellipsis whitespace-nowrap text-[#424242]">
-            한 사람의 따뜻한 선택이, 누군가의 내일을 다시...
+            한 사람의 따뜻한 선택이, 누군가의 내일을 다시 뛰게 합니다.
           </p>
           <p className="w-full cursor-pointer overflow-hidden px-[18px] py-[10px] text-[19px] overflow-ellipsis whitespace-nowrap text-[#424242]">
-            한 사람의 따뜻한 선택이, 누군가의 내일을 다시...
+            한 사람의 따뜻한 선택이, 누군가의 내일을 다시 뛰게 합니다.
           </p>
           <p className="w-full cursor-pointer overflow-hidden px-[18px] py-[10px] text-[19px] overflow-ellipsis whitespace-nowrap text-[#424242]">
-            한 사람의 따뜻한 선택이, 누군가의 내일을 다시...
+            한 사람의 따뜻한 선택이, 누군가의 내일을 다시 뛰게 합니다.
           </p>
           <p className="w-full cursor-pointer overflow-hidden px-[18px] py-[10px] text-[19px] overflow-ellipsis whitespace-nowrap text-[#424242]">
-            한 사람의 따뜻한 선택이, 누군가의 내일을 다시...
+            한 사람의 따뜻한 선택이, 누군가의 내일을 다시 뛰게 합니다.
           </p>
           <p className="w-full cursor-pointer overflow-hidden px-[18px] py-[10px] text-[19px] overflow-ellipsis whitespace-nowrap text-[#424242]">
-            한 사람의 따뜻한 선택이, 누군가의 내일을 다시...
+            한 사람의 따뜻한 선택이, 누군가의 내일을 다시 뛰게 합니다.
           </p>
         </div>
       </section>
