63a7d8d7ccabbac708842f30ae49f0b85f29b4a8
j0n0m2 <dbsk8223@naver.com>
2025-06-03 18:43:06 +0900

    style : 기증자추모 섹션 뱃지 스타일 변경

--- src/pages/home/components/MemorialBadge.tsx
+++ src/pages/home/components/MemorialBadge.tsx
@@ -1,15 +1,37 @@
 interface MemorialBadgeProps {
-  name: string
+  donor: {
+    name: string
+    age: number
+    gender: string
+  }
 }
 
-const MemorialBadge = ({ name }: MemorialBadgeProps) => {
+const FlowerBlob = ({ className = '' }: { className?: string }) => (
+  <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 140 146" fill="none">
+    <path
+      d="M56.8617 5.71C64.1871 -1.37664 75.8129 -1.37664 83.1383 5.71C87.3273 9.76246 93.152 11.655 98.923 10.8388C109.015 9.41133 118.42 16.2447 120.181 26.2837C121.188 32.0245 124.788 36.9793 129.937 39.711C138.94 44.488 142.533 55.5447 138.057 64.7015C135.497 69.9378 135.497 76.0622 138.057 81.2985C142.533 90.4553 138.94 101.512 129.937 106.289C124.788 109.021 121.188 113.976 120.181 119.716C118.42 129.755 109.015 136.589 98.923 135.161C93.152 134.345 87.3273 136.238 83.1383 140.29C75.8129 147.377 64.1871 147.377 56.8617 140.29C52.6727 136.238 46.848 134.345 41.077 135.161C30.9852 136.589 21.5798 129.755 19.8188 119.716C18.8118 113.976 15.212 109.021 10.0634 106.289C1.05989 101.512 -2.53265 90.4553 1.94345 81.2985C4.50309 76.0622 4.50309 69.9378 1.94346 64.7015C-2.53265 55.5447 1.05989 44.488 10.0634 39.711C15.212 36.9793 18.8118 32.0245 19.8188 26.2837C21.5798 16.2447 30.9852 9.41133 41.077 10.8388C46.848 11.655 52.6727 9.76245 56.8617 5.71Z"
+      fill="#E0F5E6"
+    />
+  </svg>
+)
+
+const MemorialBadge = ({ donor }: MemorialBadgeProps) => {
   return (
-    <div className="flex h-[120px] w-[120px] flex-col items-center justify-center rounded-full bg-[#353B40]">
-      <img src="/ribbon.png" alt="Memorial Ribbon" width={54} />
-      <p className="text-[19px] font-semibold text-white">
-        {name}
-        <span className="ml-1 font-normal">님</span>
-      </p>
+    <div className="relative h-[146px] w-[140px]">
+      {/* 배경 SVG */}
+      <FlowerBlob className="absolute inset-0 h-full w-full" />
+      <div className="absolute top-[50%] left-[50%] h-[110px] w-[110px] translate-x-[-50%] translate-y-[-50%] rounded-full bg-white shadow-[0_0px_20px_0px_rgb(147,220,168)]"></div>
+      {/* 오버레이 내용 */}
+      <div className="absolute inset-0 flex flex-col items-center justify-center">
+        <img src="/ribbon.png" alt="Memorial Ribbon" width={42} />
+        <p className="text-[17px] font-semibold text-gray-800">
+          {donor.name}
+          <span className="ml-1 font-normal">님</span>
+        </p>
+        <p className="text-gray-60 mt-[-4px] text-[13px] font-bold">
+          {donor.gender},{donor.age}
+        </p>
+      </div>
     </div>
   )
 }
--- src/pages/home/components/MemorialSection.tsx
+++ src/pages/home/components/MemorialSection.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import MemorialBadge from '@/pages/home/components/MemorialBadge'
 
 const dummyData = [
-  { id: 1, name: '홍길동', date: '2023-01-01', message: '생명나눔에 동참해 주셔서 감사합니다.' },
-  { id: 2, name: '김영희', date: '2023-02-01', message: '고귀한 뜻을 잊지 않겠습니다.' },
-  { id: 3, name: '이철수', date: '2023-03-01', message: '생명나눔을 실천해 주셔서 감사합니다.' },
-  { id: 4, name: '박지민', date: '2023-04-01', message: '고귀한 뜻을 기억하겠습니다.' },
-  { id: 5, name: '최수영', date: '2023-05-01', message: '생명나눔에 감사드립니다.' },
+  { id: 1, name: '홍길동', date: '2023-01-01', age: 30, gender: '남' },
+  { id: 2, name: '김영희', date: '2023-02-01', age: 20, gender: '여' },
+  { id: 3, name: '이철수', date: '2023-03-01', age: 30, gender: '남' },
+  { id: 4, name: '박지민', date: '2023-04-01', age: 24, gender: '여' },
+  { id: 5, name: '최수영', date: '2023-05-01', age: 32, gender: '남' },
 ]
 
 const MemorialSection: React.FC = () => {
   return (
-    <section className="bg-gray-5 min-w-[680px] grow-1 rounded-[40px] px-[40px] py-8">
+    <section className="border-gray-20 min-w-[680px] grow-1 rounded-[40px] border-1 px-[40px] py-8">
       <a className="mb-1 inline-flex cursor-pointer items-center gap-3" href="/memorial">
         <h2 className="text-gray-95 inline text-2xl font-bold">기증자 추모관</h2>
         <img src="/public/icon/Arrow.svg" alt="" width={24} height={24} />
@@ -21,7 +21,7 @@ const MemorialSection: React.FC = () => {
       </p>
       <div className="grid grid-cols-5 gap-[18px]">
         {dummyData.map((donor) => {
-          return <MemorialBadge key={donor.id} name={donor.name} />
+          return <MemorialBadge key={donor.id} donor={donor} />
         })}
       </div>
     </section>
